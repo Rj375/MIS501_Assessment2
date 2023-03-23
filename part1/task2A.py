b99dda1e46ce0ba0fb03ab7b021bdc24d4091623
Ramesh Giri <rameshgiri@Rameshs-MacBook.local>
2023-03-23 21:31:27 +1030

    solved minor bug oF user leaving the inputs empty

--- part1/task2A.py
+++ part1/task2A.py
@@ -22,68 +22,74 @@ while i == 0:
         confirmPassword = input("Re-enter your password:- ")
         dateOfBirth = input("Enter a date of birth (DD/MM/YYYY) No Space:- ")
 
-        yearOfBirth = dateOfBirth[-4:]
-        convertedYearOfBirth = int(yearOfBirth)
-        age = currentYear - convertedYearOfBirth
-        
-        numericValueAtTheEnd = password[-1].isdigit()
-   
-
-        if mobile.find('0') == -1:
-            print("Mobile number must contain 0 at the beginning")
-        elif mobile.index('0') != 0:
-            print("Mobile number must start with 0")
-        elif len(mobile) != 10:
-            print("Mobile number must not be less than or greater than 10 digits")
-        elif not dateFormat.match(dateOfBirth):
-            print("You have entered a date of birth in invalid format. Please try again.")
-        elif password.find('@') == -1:
-            print("Password must include @")
-        elif numericValueAtTheEnd == False:
-            print("Password must contain a number at the end")
-        elif password != confirmPassword:
-            print("Your passwards are not matching. Please try again:")
-        elif age < 21:
-            print("You must be at least 21 years old to Sign Up")
-            break
-        else:
-            data.append(name)
-            data.append(mobile)
-            data.append(password)
-            data.append(dateOfBirth)
-            print("You have Successfully Signed Up")
+        if name == "" and mobile == "" and password == "" and confirmPassword == "" and dateOfBirth == "":
+            print("Inputs cannot be empty, please enter all the inputs")
+        else:    
+            yearOfBirth = dateOfBirth[-4:]
+            convertedYearOfBirth = int(yearOfBirth)
+            age = currentYear - convertedYearOfBirth
+            numericValueAtTheEnd = password[-1].isdigit()
+    
+            if mobile.find('0') == -1:
+                print("Mobile number must contain 0 at the beginning")
+            elif mobile.index('0') != 0:
+                print("Mobile number must start with 0")
+            elif len(mobile) != 10:
+                print("Mobile number must not be less than or greater than 10 digits")
+            elif not dateFormat.match(dateOfBirth):
+                print("You have entered a date of birth in invalid format. Please try again.")
+            elif password.find('@') == -1:
+                print("Password must include @")
+            elif numericValueAtTheEnd == False:
+                print("Password must contain a number at the end")
+            elif password != confirmPassword:
+                print("Your passwards are not matching. Please try again:")
+            elif age < 21:
+                print("You must be at least 21 years old to Sign Up")
+                break
+            else:
+                data.append(name)
+                data.append(mobile)
+                data.append(password)
+                data.append(dateOfBirth)
+                print("You have Successfully Signed Up")
     elif userInput == '2':
         if len(data) == 0:
             print("You have not Signed Up, Please Sign Up First")
         else:
             username = input("Please enter your Username (Mobile Number):- ")
             loginPassword = input("Please enter your Password:- ")
-
-            if username == data[1] and loginPassword == data[2]:
-                print("You have Successfully Signed In.")
-                print("Welcome,",data[0])
-                print(resetPassword)
-                print(signOut)
-                loggedUserInput = input("Enter 1 or 2:- ")
-
-                if loggedUserInput == '1':
-                    resetUserInput = input("Please enter your Username (Mobile Number):- ")
-                    oldPassword = input("Please enter your old password:- ")
-                    if username == data[1] and loginPassword == data[2] and oldPassword == data[2]:
-                        newPassword = input("Please enter your new password:- ")
-                        if newPassword == data[2]:
-                            print("You can not re-enter your old password, Please choose new one")
-                        else:
-                            data[2] = newPassword
-                            print("Your password has been reset successlly")
-                    else:
-                        print("The inFormation you provided is not valid")
-                elif loggedUserInput == '2':
-                    print("You are Successlly Signed Out.")
-                else:
-                    print("Please enter valid input eg:- 1 or 2.")
+            if username == "" and loginPassword == "":
+                print("Inputs cannot be empty, please enter all the inputs")
             else:
-                print("You have not Sign up with this contact number, Please Sign Up First.")
+                if username == data[1] and loginPassword == data[2]:
+                    print("You have Successfully Signed In.")
+                    print("Welcome,",data[0])
+                    print(resetPassword)
+                    print(signOut)
+                    loggedUserInput = input("Enter 1 or 2:- ")
+
+                    if loggedUserInput == '1':
+                        resetUserInput = input("Please enter your Username (Mobile Number):- ")
+                        oldPassword = input("Please enter your old password:- ")
+                        if resetUserInput == "" and oldPassword == "":
+                            print("Inputs cannot be empty, please enter all the inputs")
+                        else:    
+                            if username == data[1] and loginPassword == data[2] and oldPassword == data[2]:
+                                newPassword = input("Please enter your new password:- ")
+                                if newPassword == data[2]:
+                                    print("You can not re-enter your old password, Please choose new one")
+                                else:
+                                    data[2] = newPassword
+                                    print("Your password has been reset successlly")
+                            else:
+                                print("The inFormation you provided is not valid")
+                    elif loggedUserInput == '2':
+                        print("You are Successlly Signed Out.")
+                    else:
+                        print("Please enter valid input eg:- 1 or 2.")
+                else:
+                    print("You have not Sign up with this contact number, Please Sign Up First.")
 
     elif userInput == '3':
         print("Thank You for using the Application")
