4fd340c10484b395b36821b9d2b18174588818b1
Ramesh Giri <rameshgiri@Rameshs-MacBook.local>
2023-03-23 23:28:26 +1030

    Fixed minor validations

--- part1/task2A.py
+++ part1/task2A.py
@@ -24,12 +24,12 @@ while i == 0:
 
         if name == "" and mobile == "" and password == "" and confirmPassword == "" and dateOfBirth == "":
             print("Inputs cannot be empty, please enter all the inputs")
-        else:    
+        else:
             yearOfBirth = dateOfBirth[-4:]
             convertedYearOfBirth = int(yearOfBirth)
             age = currentYear - convertedYearOfBirth
             numericValueAtTheEnd = password[-1].isdigit()
-    
+
             if mobile.find('0') == -1:
                 print("Mobile number must contain 0 at the beginning")
             elif mobile.index('0') != 0:
@@ -59,12 +59,13 @@ while i == 0:
         else:
             j = 0
             for login in range(3):
-                username = input("Please enter your Username (Mobile Number):- ")
+                username = input(
+                    "Please enter your Username (Mobile Number):- ")
                 loginPassword = input("Please enter your Password:- ")
                 if username == "" and loginPassword == "":
                     print("Inputs cannot be empty, please enter all the inputs")
                 else:
-                    if username != data[1]: 
+                    if username != data[1]:
                         print("Invalid Username, Please try again.")
                     if loginPassword != data[2]:
                         print("Invalid password, Please try again.")
@@ -73,20 +74,29 @@ while i == 0:
                             print("You have used maximum attempts oF login, please reset the password.")
                             isUsername = input("Please enter your Username (Mobile Number):- ")
                             isDateOfBirth = input("Enter a date of birth (DD/MM/YYYY) No Space:- ")
-                            if isUsername == data[1] and isDateOfBirth == data[3]:
-                                newResetPassword = input("Please enter your new password:- ")
-                                confirmNewResetPassword = input("Please re-enter the new password:- ")
-                                if newResetPassword == confirmNewResetPassword:
-                                    print("Your password has been reset successfully")
-                                    data[2] = newResetPassword
-                                else:
-                                    print("Passwords do not match with each other.")
-                                    break
+                            if isUsername == "" and isDateOfBirth == "":
+                                print("Inputs cannot be empty, please enter all the inputs")
                             else:
-                                print("The data you entered are invalid")
+                                if isUsername == data[1] and isDateOfBirth == data[3]:
+                                    newResetPassword = input("Please enter your new password:- ")
+                                    confirmNewResetPassword = input("Please re-enter the new password:- ")
+                                    if newResetPassword.find('@') == -1:
+                                        print("Password must include @")
+                                    elif newResetPassword[-1].isdigit() == False:
+                                        print("Password must contain a number at the end")
+                                    elif newResetPassword == data[2]:
+                                        print("You can not re-enter your old password, Please choose new one")
+                                    elif newResetPassword == confirmNewResetPassword:
+                                        print("Your password has been reset successfully")
+                                        data[2] = newResetPassword
+                                    else:
+                                        print("Passwords do not match with each other.")   
+                                        break
+                                else:
+                                    print("The data you entered are invalid")
                     else:
                         print("You have Successfully Signed In.")
-                        print("Welcome,",data[0])
+                        print("Welcome,", data[0])
                         print(resetPassword)
                         print(signOut)
                         loggedUserInput = input("Enter 1 or 2:- ")
@@ -96,11 +106,15 @@ while i == 0:
                             oldPassword = input("Please enter your old password:- ")
                             if resetUserInput == "" and oldPassword == "":
                                 print("Inputs cannot be empty, please enter all the inputs")
-                            else:    
+                            else:
                                 if username == data[1] and loginPassword == data[2] and oldPassword == data[2]:
                                     newPassword = input("Please enter your new password:- ")
-                                    if newPassword == data[2]:
-                                        print("You can not re-enter your old password, Please choose new one")
+                                    if newPassword.find('@') == -1:
+                                       print("Password must include @")
+                                    elif newPassword[-1].isdigit() == False:
+                                       print("Password must contain a number at the end")
+                                    elif newPassword == data[2]:
+                                       print("You can not re-enter your old password, Please choose new one")
                                     else:
                                         data[2] = newPassword
                                         print("Your password has been reset successFully")
@@ -110,13 +124,9 @@ while i == 0:
                             print("You are Successlly Signed Out.")
                         else:
                             print("Please enter valid input eg:- 1 or 2.")
-                    
 
     elif userInput == '3':
         print("Thank You for using the Application")
-        break    
+        break
     else:
         print("Please enter valid input eg:- 1, 2 or 3")
-       
-        
-        
